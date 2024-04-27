6dc8ee3a55e54d933ec59169f80242eab7e83b54
Murshed J. Ahmed <mahmed@marvel.com>
2024-04-27 17:28:23 -0400

    Created Vertex class

---
+++ src/Vertex.java
@@ -0,0 +1,23 @@
+public class Vertex {
+    private String name;
+    private int x;
+    private int y;
+
+    public Vertex(String name, int x, int y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
