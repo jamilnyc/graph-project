1728e3476b7e87fb2e131e26f3ccd8d9e0f205bd
Murshed J. Ahmed <mahmed@marvel.com>
2024-04-28 17:37:21 -0400

    Add equals() and toString() methods to Vertex Class

--- src/Vertex.java
+++ src/Vertex.java
@@ -20,4 +20,33 @@ public class Vertex {
     public int getY() {
         return y;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Objects are equal to themselves
+        if (this == obj) {
+            return true;
+        }
+
+        // Objects can't be equal to null
+        if (obj == null) {
+            return false;
+        }
+
+        // Can't compare to objects of a different class
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // Vertices are equal if they have the same name and coordinates
+        Vertex other = (Vertex) obj;
+        return this.getName().equals(other.getName())
+                && this.getX() == other.getX()
+                && this.getY() == other.getY();
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex [" + name + ", " + x + ", " + y + "]";
+    }
 }
