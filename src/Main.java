717627fb99e8ee3511894e257957725b5f42faa9
Murshed J. Ahmed <mahmed@marvel.com>
2024-04-28 17:47:56 -0400

    Create basic Main class with DFS test

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        Vertex vA = new Vertex("A", 0, 0);
+        Vertex vB = new Vertex("B", 1, 1);
+        Vertex vC = new Vertex("C", 2, 2);
+        Vertex vD = new Vertex("D", 3, 3);
+        Vertex vE = new Vertex("E", 4, 4);
+        Vertex vF = new Vertex("F", 5, 5);
+
+        g.addVertex(vA);
+        g.addVertex(vB);
+        g.addVertex(vC);
+        g.addVertex(vD);
+        g.addVertex(vE);
+        g.addVertex(vF);
+
+        g.addEdge(vA, vF);
+        g.addEdge(vA, vE);
+        g.addEdge(vA, vB);
+        g.addEdge(vB, vD);
+        g.addEdge(vD, vC);
+
+        System.out.println(g.getDepthFirstSearch(vA));
+
+    }
+}
